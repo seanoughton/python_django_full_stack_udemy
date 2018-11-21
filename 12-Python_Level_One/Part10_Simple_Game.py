277df74a4d38ae76bd008eec4b19c29a2a24e090
seanoughton <seanoughton@gmail.com>
2018-11-20 16:13:34 -0800

    simple game solution

--- 12-Python_Level_One/Part10_Simple_Game.py
+++ 12-Python_Level_One/Part10_Simple_Game.py
@@ -23,13 +23,48 @@
 
 # Try to figure out what this code is doing and how it might be useful to you
 import random
-digits = list(range(10))
-random.shuffle(digits)
-print(digits[:3])
 
-# Another hint:
-guess = input("What is your guess? ")
-print(guess)
+
+def get_guess():
+    return list(input("What is your guess? "))
+
+def generate_random():
+    digits = [str(num) for num in range(10)]
+    random.shuffle(digits)
+    print(digits[:3])
+    return digits[:3]
+
+def generate_clues(numbers,guess):
+    if guess == numbers:
+        return "You guessed correctly"
+    clues = []
+
+    for ind,num in enumerate(guess):
+        if num == numbers[ind]:
+            clues.append("Match")
+        elif num in numbers:
+            clues.append("Close")
+    if clues == []:
+        return ["Nope"]
+    else:
+        return clues
+
+
+def run_game():
+    numbers = generate_random()
+    clue_report = []
+    while clue_report != "You guessed correctly":
+        guess = get_guess()
+        clue_report = generate_clues(numbers,guess)
+        print ("here is the result of your guess: ")
+        for clue in clue_report:
+            print(clue)
+
+
+run_game()
+
+
+
 
 # Think about how you will compare the input to the random number, what format
 # should they be in? Maybe some sort of sequence? Watch the Lecture video for more hints!
