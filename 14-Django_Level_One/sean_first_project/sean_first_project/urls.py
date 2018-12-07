c6d9406b23d26d408e0cb72f2bf647ce8ee323cd
seanoughton <seanoughton@gmail.com>
2018-12-06 16:59:17 -0800

    changing the url routing so that each app has it's own urls.py file

--- 14-Django_Level_One/sean_first_project/sean_first_project/urls.py
+++ 14-Django_Level_One/sean_first_project/sean_first_project/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 from sean_first_app import views
 
 urlpatterns = [
     # path('',views.index),
-    url(r'^$',views.index,name=index),
+    url(r'^$',views.index,name='index'),
+    url(r'^sean_first_app/',include('sean_first_app.urls')),
     path('admin/', admin.site.urls),
 ]
