756ca4c0247ed52b34897f77f0044a1b534ecd31
seanoughton <seanoughton@gmail.com>
2019-01-17 14:58:44 -0800

    starting to set up views file

--- 16-Django_Level_Three/seanforms/seanformapp/views.py
+++ 16-Django_Level_Three/seanforms/seanformapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+
+def index(request):
+    return render(request,'seanformapp/index.html')
+
+def form_name_view(request):
+    form = forms.FormName()
+    return render(request,'seanformapp/form_page.html',{'form':form})
