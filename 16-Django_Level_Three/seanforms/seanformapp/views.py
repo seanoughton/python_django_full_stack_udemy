03722230530b3094ce64ba19c340a62ce558c0ba
seanoughton <seanoughton@gmail.com>
2019-01-17 16:11:05 -0800

    created validators for the form data

--- 16-Django_Level_Three/seanforms/seanformapp/views.py
+++ 16-Django_Level_Three/seanforms/seanformapp/views.py
@@ -7,4 +7,14 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
+
+    if request.method == "POST":
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("VALIDATION SUCCESS")
+            print("name:" + form.cleaned_data['name'])
+            print("email:" + form.cleaned_data['email'])
+            print("text:" + form.cleaned_data['text'])
+
     return render(request,'seanformapp/form_page.html',{'form':form})
