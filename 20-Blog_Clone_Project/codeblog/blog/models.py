7672ca6256b6c942a6fe87b934232c4200bfb517
seanoughton <seanoughton@gmail.com>
2019-01-25 15:56:23 -0800

    created basic forms.py file

--- 20-Blog_Clone_Project/codeblog/blog/models.py
+++ 20-Blog_Clone_Project/codeblog/blog/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 from django.urls import reverse
 
 # Create your models here.
-class Post(model.Models):
-    author = models.ForeignKey('auth.user')
+class Post(models.Model):
+    author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
     text = models.TextField()
     title = models.CharField(max_length=200)
-    create_date = models.DateTimeField(default=timezone.new())
+    create_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
@@ -17,21 +17,27 @@ class Post(model.Models):
     def approve_comments(self):
         return self.comments.filter(approved_comment=True)
 
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
     def __str__():
         return self.title
 
 
-class Comment(model.Models):
-    post = models.ForeignKey('blog.Post',related_name="comments")
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name="comments",on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.new())
+    create_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
 
 
     def approve(self):
         self.approved_comment = True
         self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
 
     def __str__():
         return self.text
