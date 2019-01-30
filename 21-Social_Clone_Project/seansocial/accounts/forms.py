95288fff512a64277d6c0ba5dbfbcf216848eaed
seanoughton <seanoughton@gmail.com>
2019-01-30 13:09:39 -0800

    created user login and logout capabilities

--- 21-Social_Clone_Project/seansocial/accounts/forms.py
+++ 21-Social_Clone_Project/seansocial/accounts/forms.py
@@ -1,16 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-class UserCreateForm(UserCreationForm):
 
-    #THE METACLASS CONNECTS THE FORM TO THE MODEL
+class UserCreateForm(UserCreationForm):
     class Meta:
-        fields = ('username','email','password1','password2')
-        #the model comes from the baked in model given to you by the contrib.auth library
+        fields = ("username", "email", "password1", "password2")
         model = get_user_model()
 
-    def __init__(self,*args,**kwargs):
-        super().__init(*args,**kwargs)
-        #THIS SETS UP THE LABELS FOR THE FORM FIELDS BASED ON THE MODEL FIELDS
-        self.fields['username'].label = "Display Name"
-        self.fields['email'].label = "Email Address"
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].label = "Display name"
+        self.fields["email"].label = "Email address"
--- 21-Social_Clone_Project/seansocial/accounts/models.py
+++ 21-Social_Clone_Project/seansocial/accounts/models.py
@@ -5,5 +5,5 @@ from django.contrib import auth
 class User(auth.models.User,auth.models.PermissionsMixin):
 
 
-    def __str(self):
+    def __str__(self):
         return "@{}".format(self.username)
