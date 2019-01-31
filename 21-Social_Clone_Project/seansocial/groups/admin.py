e6bf6d9f42c7f6f5a7013db18463adc4a82f99e9
seanoughton <seanoughton@gmail.com>
2019-01-31 14:31:36 -0800

    created views and url for posts

--- 21-Social_Clone_Project/seansocial/groups/admin.py
+++ 21-Social_Clone_Project/seansocial/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+#allows you to use the admin interface with ability to edit models on the same page as the parent model => group member => parent is Group
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
